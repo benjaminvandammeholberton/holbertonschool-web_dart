18959f798aa5d805d5635cc42fd9d9dac5a0d040
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-07-01 15:06:18 +0200

    add: task 2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,15 @@
+class Password{
+  var password;
+
+  bool isValid() {
+  
+  final RegExp regex = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,16}$');
+  return regex.hasMatch(this.password);
+
+  }
+
+  @override
+  String toString(){
+    return "Your password is ${this.password}";
+  }
+}
