066fda468df643b6c3c76240cb62974ab3c8e865
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-07-01 09:17:51 +0200

    fix: task 5

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,8 +1,5 @@
 void main(List<String> args) {
-  /*
-  Write Your code below 
-   */
   var nb = int.parse(args[0]);
-  assert(nb>=80, "The score must be bigger or equal to 80");
+  assert(nb >= 80, "The score must be bigger or equal to 80");
   print("You passed");
 }
