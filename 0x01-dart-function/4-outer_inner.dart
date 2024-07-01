07d011387f997f0d60aeabf5d368bfeed57821b9
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-07-01 10:52:02 +0200

    fix: task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -2,7 +2,7 @@ void outer(String name, String id){
   String inner(){
     var firstLetter = name.substring(0,1);
     var lastName = name.split(' ')[1];
-    return('Hello Agent $firstLetter.$lastName, your id is $id');
+    return('Hello Agent $firstLetter.$lastName your id is $id');
   }
   print(inner());
 }
