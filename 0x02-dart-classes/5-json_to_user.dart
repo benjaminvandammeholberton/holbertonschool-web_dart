a51745e07eed03a7a34295fee3bc6ff399372ba7
benjaminvandammeholberton <6206@holbertonstudents.com>
2024-07-03 10:19:46 +0200

    add: task 5

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,27 +1,26 @@
-class User{
+class User {
   String name;
   int age;
   double height;
   int id;
 
-  User({required this.name, required this.age, required this.height, required this.id}){
-  }
+  User({required this.name, required this.age, required this.height, required this.id});
 
   static User fromJson(Map<dynamic, dynamic> userJson){
-    return User(name: userJson['name'], age: userJson['age'], id: userJson['id'], height: userJson['height']);
+    return User(name: userJson['name'], id: userJson['id'], height: userJson['height'], age: userJson['age']);
   }
 
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toJson(){
     return {
-      "id": this.id,
-      "name": this.name,
-      "age": this.age,
-      "height": this.height,
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
     };
   }
 
   @override
-  String toString() {
-    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})';
+  String toString(){
+    return 'id : $id ,name: $name, age: $age, height: $height';
   }
 }
